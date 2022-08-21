17bdb37bee195bf2adddd3f2685ab86cb06225f1
daniil <afpij>
2022-08-21 12:55:27 +0200

    CRUD done

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
+    path('create-form/', views.create_room, name='create-room'),
+    path('update-form/<str:pk>/', views.update_form, name='update-form'),
+    path('delete-form/<str:pk>/', views.delete_form, name='delete-form'),
 ]
